80bb38166811900d60f5eb9b3edd42a43b4313f4
Tomas Michalek <michto01@gmail.com>
2022-02-10 11:21:24 +0000

    Kod z hodiny

--- main.c
+++ main.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello World\n");
+/**
+  Napisme funkci pro vypocet obsahu ctverce
+ */
+// TADY 
+float obsah_ctverce(float strana) {
+  return strana * strana;
+}
+
+//TADY
+/**
+  Napisme funkci pro vypocet obvodu obdelniku
+ */
+float obvod_obdelniku(float a, float b) {
+  return 2 * a + 2 * b;
+}
+
+/**
+ Napis funkci ktera nakresli obdelni pomoci '*'
+ */
+void kresli_obdelnik (int a, int b) {
+  for (int i = a; i > 0; i--) {
+    for (int j = b; j > 0; j--) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
+}
+
+
+void hlaska(void);  // Prototyp funkce
+
+void pause(void) {
+  hlaska();
+  getchar(); // LF
+  getchar(); // CR
+}
+
+void hlaska(void) {
+  printf("Pro ukonceni programu, zmacni jakoukoliv klavesu...\n");
+}
+
+
+int main() {
+  //kresli_obdelnik(30, 20);
+  printf("Vysledek: %f", obvod_obdelniku(3,3));
+#if 0
+  float i, obsah;
+  printf("zadej stranu: ");
+  scanf("%f", &i);
+  obsah = obsah_ctverce(i);
+  printf("\nHodnota: %f\n", obsah);
+  pause();
+#endif
   return 0;
 }
